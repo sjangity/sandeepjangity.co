5036e522a7b9d62e61dc9d3341e02d81cfef296b
Sandeep Jangity <13264604+sjangity@users.noreply.github.com>
2019-06-04 19:21:57 -0700

    use route exact

--- code/src/PortfolioMaster.js
+++ code/src/PortfolioMaster.js
@@ -22,8 +22,8 @@ class PortfolioMaster extends Component {
             </Typography>
           </Container>
 
-          <Route path="/" component={MasterContent} />
-          <Route path="/detail" component={DetailContent} />
+          <Route exact path="/" component={MasterContent} />
+          <Route exact path="/detail" component={DetailContent} />
 
           {/* <RouteRenderer path="/" component={MasterContent} />
           <RouteRenderer path="/detail" component={DetailContent} /> */}
